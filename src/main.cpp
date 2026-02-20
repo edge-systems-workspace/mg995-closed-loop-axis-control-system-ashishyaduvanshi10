3284d410c9d1375535839b242c7ae31cf41aed32
ashish.yadav_cs.aiml24@gla.ac.in <ashishkumar12333ak@gmail.com>
2026-02-20 11:49:28 +0530

    Add servo and ultrasonic setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <Arduino.h>
+#include <Servo.h>
+
+Servo MYSERVO;
+
+int trigPin = 9;
+int echoPin = 10;
 
 void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  MYSERVO.attach(5);
+  Serial.begin(9600);
 }
 
 void loop() {
